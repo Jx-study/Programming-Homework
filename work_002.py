2e2e118615687815a0d0f7739512a6407520790c
Jx-study <145241060+Jx-study@users.noreply.github.com>
2023-12-11 22:36:14 +0800

    Delete work_002.py

--- work_002.py
+++
@@ -1,9 +0,0 @@
-import math
-a = int(input())
-b = int(input())
-c = int(input())
-
-x1 = ((-b)+math.sqrt(b*b-4*a*c))/(2*a)
-x2 = ((-b)-math.sqrt(b*b-4*a*c))/(2*a)
-print("\n%.1f" %x1)
-print("%.1f" %x2)
